f6c7ed60815693bb7863fb04273e4d61305ceb3c
MiPersona <bgva2005@yahoo.com>
2020-06-06 12:53:25 -0500

    0001_EstructuraInicial

---
+++ src/components/formik/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class PaginaFormik extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <h1>Página Formik</h1>
+      </div>
+    );
+  }
+}
+
+export default PaginaFormik;
---
+++ src/components/inicial/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class PruebaInicial extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <h1>Hola Mundo</h1>
+      </div>
+    );
+  }
+}
+
+export default PruebaInicial;
